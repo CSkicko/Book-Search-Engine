741dd1db9b67dc8e0b0350853e7ba5c3c5d626aa
Craig <craig.skicko@gmail.com>
2022-10-12 20:51:13 +1100

    Refactored server code to use apollo server and the defined typedefs and resolvers

--- server/server.js
+++ server/server.js
@@ -3,6 +3,14 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// Import dependencies and set up apollo server
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -16,6 +24,18 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Create a function to initiate the apollo server connection and start the app
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+      console.log(`GraphQL URL: http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+}
+
+// Start the apollo server by calling the function
+startApolloServer(typeDefs, resolvers);
