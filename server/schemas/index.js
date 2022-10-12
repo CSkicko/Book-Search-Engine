f19f9e7d875fd321e7395e064441c92cce859da9
Craig <craig.skicko@gmail.com>
2022-10-12 19:41:58 +1100

    Created type definitions based on user and book models

---
+++ server/schemas/index.js
@@ -0,0 +1,5 @@
+// File to gather type definitions and resolvers to be imported into the server 
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
